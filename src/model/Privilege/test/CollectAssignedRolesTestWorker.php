c4275c59d9d3ad12244de153d65e373c1ec51f3e
Nabel Hernandez <nabelhm@cubalider.com>
2015-07-03 16:28:55 -0400

    Created 4 CollectTestWorkers for check the state of the system before and after creates an account

---
+++ src/model/Privilege/test/CollectAssignedRolesTestWorker.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Muchacuba\Privilege;
+
+use Muchacuba\Privilege\AssignedRole\ConnectToStorageInternalWorker;
+use JMS\DiExtraBundle\Annotation as Di;
+
+/**
+ * @author Nabel Hernandez <nabelhm@cubalider.com>
+ * @author Yosmany Garcia <yosmanyga@gmail.com>
+ *
+ * @Di\Service()
+ */
+class CollectAssignedRolesTestWorker
+{
+    /**
+     * @var ConnectToStorageInternalWorker
+     */
+    private $connectToStorageInternalWorker;
+
+    /**
+     * @param ConnectToStorageInternalWorker $connectToStorageInternalWorker
+     *
+     * @Di\InjectParams({
+     *     "connectToStorageInternalWorker" = @Di\Inject("muchacuba.privilege.assigned_role.connect_to_storage_internal_worker")
+     * })
+     */
+    function __construct(
+        ConnectToStorageInternalWorker $connectToStorageInternalWorker
+    )
+    {
+        $this->connectToStorageInternalWorker = $connectToStorageInternalWorker;
+    }
+
+    /**
+     * Collects internet profiles.
+     *
+     * @return \Iterator
+     */
+    public function collect()
+    {
+        return $this->connectToStorageInternalWorker->connect()
+            ->find()
+            ->fields([
+                '_id' => 0 // Exclude
+            ]);
+    }
+}
