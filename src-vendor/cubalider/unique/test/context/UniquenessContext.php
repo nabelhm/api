6b14e6d5add85804c5f4a8a6fbff799606d69b0b
Nabel Hernandez <nabelhm@cubalider.com>
2015-07-11 00:08:38 -0400

    Added the method for checking the state of the system

--- src-vendor/cubalider/unique/test/context/UniquenessContext.php
+++ src-vendor/cubalider/unique/test/context/UniquenessContext.php
@@ -2,11 +2,16 @@
 
 namespace Cubalider\Unique;
 
+use Behat\Behat\Context\Environment\InitializedContextEnvironment;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Symfony2Extension\Context\KernelAwareContext;
-use Cubalider\ConnectToStorageWorker as ConnectToUniquenessStorageWorker;
+use Cubalider\Unique\Uniqueness as ConnectToUniquenessStorageWorker;
 use Symfony\Component\HttpKernel\KernelInterface;
+use PHPUnit_Framework_Assert as Assert;
+use Coduo\PHPMatcher\Factory\SimpleFactory;
+use Muchacuba\Context as RootContext;
 
 /**
  * @author Nabel Hernandez <nabelhm@cubalider.com>
@@ -20,11 +25,29 @@ class UniquenessContext implements SnippetAcceptingContext, KernelAwareContext
     private $kernel;
 
     /**
+     * @var RootContext
+     */
+    private $rootContext;
+
+    /**
      * {@inheritdoc}
      */
     public function setKernel(KernelInterface $kernel)
     {
         $this->kernel = $kernel;
+    }
+
+    /**
+     * @BeforeScenario
+     *
+     * @param BeforeScenarioScope $scope
+     */
+    public function gatherRootContext(BeforeScenarioScope $scope)
+    {
+        /** @var InitializedContextEnvironment $environment */
+        $environment = $scope->getEnvironment();
+
+        $this->rootContext = $environment->getContext('Muchacuba\Context');
     }
 
     /**
@@ -52,5 +75,25 @@ class UniquenessContext implements SnippetAcceptingContext, KernelAwareContext
                 $item['id']
             );
         }
+    }
+
+    /**
+     * @Given the system should have the following uniquenesses:
+     */
+    public function theSystemShouldHaveTheFollowingUniquenesses(PyStringNode $body)
+    {
+        /** @var CollectUniquenessTestWorker $collectUniquenessTestWorker */
+        $collectUniquenessTestWorker = $this->kernel
+            ->getContainer()
+            ->get('cubalider.unique.collect_uniqueness_test_worker');
+
+        Assert::assertTrue(
+            (new SimpleFactory())->createMatcher()->match(
+                iterator_to_array($collectUniquenessTestWorker->collect()),
+                (array) json_decode($body->getRaw(), true)
+            )
+        );
+
+        $this->rootContext->ignoreState('Cubalider\Uniqueness');
     }
 }
